b7faadd7dcbf7ae6edb5b60fc42edb2103686d40
MehdiBenabbou <66429447+MehdiBenabbou@users.noreply.github.com>
2024-01-04 19:10:33 +0100

    Update vols des véhicules [SQL].sql

--- Analyse des vols des véhicules/vols des véhicules [SQL].sql
+++ Analyse des vols des véhicules/vols des véhicules [SQL].sql
@@ -1,21 +1,21 @@
--- Nettoyage des données 
+-- Nettoyage des donnÃ©es 
 -- (Supprimer Les lignes vides)
 
 DELETE FROM stolen_vehicles
 WHERE vehicle_type IS NULL
 OR vehicle_desc IS NULL;
 
--- (Supprimer Les colonnes inutiles)
+-- Supprimer Les colonnes inutiles
 
 ALTER TABLE stolen_vehicles
 DROP COLUMN vehicle_desc, color;
 
-" -- Ajouter une nouvelle colonne pour stocker le nom du jour"
+-- Ajouter une nouvelle colonne pour stocker le nom du jour
 ALTER TABLE stolen_vehicles
 ADD DayOfWeek NVARCHAR(50); 
 -- Change the datatype and size as needed
 
--- Update the new column with the day names
+--  Mettre Ã  jour la nouvelle colonne avec les noms des jours
 UPDATE stolen_vehicles
 SET DayOfWeek = DATENAME(dw, [DATE]);
 
@@ -25,14 +25,14 @@ from stolen_vehicles
 group by DayOfWeek
 Order By NumStolen DESC;
 
---Type de vehicules plus souvent volés et les moins souvent volés
+--Type de vehicules les plus souvent volÃ©s et les moins souvent volÃ©s
 select vehicle_type, COUNT (*) AS NumStolen
 from stolen_vehicles
 group by vehicle_type
 Order by NumStolen DESC;
 
---Regions ont le plus e le moins de véhicules volés
-selec region, COUNT(*) AS NumStolen
+--Regions ont le plus et le moins de vÃ©hicules volÃ©s
+select region, COUNT(*) AS NumStolen
 from locations ls, stolen_vehicles sv
 where ls.location_id = sv.location_id
 group by ls.region
